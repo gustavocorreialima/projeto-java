93abf5584d1e1ad13100575edfc1d2759c98e7bd
Gustavo Porto <Gustavolima0203@gmail.com>
2024-09-26 21:05:43 -0300

    Entrada do nome do usuario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola, mundo!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        sc.close();
+        System.out.printf("Olá, %s! ", nome);
     }
 }
